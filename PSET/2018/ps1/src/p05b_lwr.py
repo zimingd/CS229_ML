efc1bc6b23ac6c3eb6c2cbf835cf10cd5b1036ee
Ziming Dong <zimingd@gmail.com>
2020-09-25 02:42:28 -0700

    problem set 1

--- PSET/2018/ps1/src/p05b_lwr.py
+++ PSET/2018/ps1/src/p05b_lwr.py
@@ -15,13 +15,29 @@ def main(tau, train_path, eval_path):
     """
     # Load training set
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+
 
     # *** START CODE HERE ***
     # Fit a LWR model
+    assert tau == 0.5
+    locally_weighted_regression = LocallyWeightedLinearRegression(tau)
+    locally_weighted_regression.fit(x_train,y_train)
+
+    y_pred = locally_weighted_regression.predict(x_eval)
+
     # Get MSE value on the validation set
+    mean_squared_error = ((y_eval-y_pred) ** 2).mean()
     # Plot validation predictions on top of training set
     # No need to save predictions
     # Plot data
+    import matplotlib.pyplot as plt
+    fig,ax = plt.subplots()
+    ax.plot(x_train, y_train, 'bx')
+    ax.plot(x_eval, y_pred, 'ro')
+    ax.set_title(f'tau = {tau}, MSE = {mean_squared_error}')
+    fig.savefig('output/p05b_lwr.png')
+
     # *** END CODE HERE ***
 
 
@@ -45,6 +61,8 @@ class LocallyWeightedLinearRegression(LinearModel):
 
         """
         # *** START CODE HERE ***
+        self.x = x 
+        self.y = y
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -58,3 +76,39 @@ class LocallyWeightedLinearRegression(LinearModel):
         """
         # *** START CODE HERE ***
         # *** END CODE HERE ***
+        m,n = self.x.shape
+        
+
+        y_pred = np.zeros(x.shape[0])
+
+
+        #there's probably a more efficient way to iterate and predict each y using batch numpy operations but my tiny brain is burned out and can't think about this anymore :(
+        for i, x_i in enumerate(x):
+            # each diagonal W is unique to the current data point(x_i or x[i]) we are attempting to predict
+            # based on the current datapoint's distance from each point in our training set (self.x[j])
+            W = np.diag( np.exp( - np.linalg.norm(x_i-self.x,  ord=2, axis=1)**2 / (2 * self.tau**2) ) )
+
+            # X (capitalized) refers to our training set, self.x
+            # only the final predication step and the earlier calculation of W makes use of our input (x)
+
+            #solve for theta via  X^T W X theta = X^T W y
+            theta = np.linalg.solve(self.x.T @ W @ self.x, self.x.T @ W @ self.y)
+            y_pred[i] = theta.T @ x_i
+
+
+
+        
+        # omiga =np.exp(-np.sum((self.x - x[188])**2/(2*self.tau**2), axis = 1))
+        # print(omiga)
+        # print(np.sum((self.x - x[188])**2/(2*self.tau**2), axis=1).shape)
+        # # print(   (np.linalg.norm(self.x - x[188], 2)**2)/(2*self.tau**2)  )
+        # print ( -(np.linalg.norm(x[188]-self.x, ord=2, axis=1)**2 )/ (2 * self.tau**2)   )
+
+        
+        # W = np.exp( -(np.linalg.norm(x[188]-self.x,  ord=2, axis=1)**2 )/ (2 * self.tau**2) )
+        # print(W)
+        # print(np.array_equal(omiga, W))
+        
+       
+        return y_pred
+        
--- PSET/2018/ps1/src/p05c_tau.py
+++ PSET/2018/ps1/src/p05c_tau.py
@@ -4,6 +4,8 @@ import util
 
 from p05b_lwr import LocallyWeightedLinearRegression
 
+import matplotlib.pyplot as plt
+
 
 def main(tau_values, train_path, valid_path, test_path, pred_path):
     """Problem 5(b): Tune the bandwidth paramater tau for LWR.
@@ -17,6 +19,10 @@ def main(tau_values, train_path, valid_path, test_path, pred_path):
     """
     # Load training set
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+    x_valid, y_valid = util.load_dataset(valid_path, add_intercept=True)
+    x_test, y_test = util.load_dataset(test_path, add_intercept=True)
+
+
 
     # *** START CODE HERE ***
     # Search tau_values for the best tau (lowest MSE on the validation set)
@@ -24,4 +30,36 @@ def main(tau_values, train_path, valid_path, test_path, pred_path):
     # Run on the test set to get the MSE value
     # Save predictions to pred_path
     # Plot data
+
+    min_mse_index = 0
+    min_mse = 999999999999
+
+    for i, tau in enumerate(tau_values):
+        locally_weighted_regression = LocallyWeightedLinearRegression(tau)
+        locally_weighted_regression.fit(x_train,y_train)
+        y_pred = locally_weighted_regression.predict(x_valid)
+        mean_squared_error = ((y_valid-y_pred) ** 2).mean()
+        
+        if(mean_squared_error < min_mse):
+            min_mse = mean_squared_error
+            min_mse_index = i
+
+        plot(tau, mean_squared_error, x_train, y_train, x_valid, y_pred)
+
+    tau = tau_values[min_mse_index]
+    locally_weighted_regression = LocallyWeightedLinearRegression(tau)
+    locally_weighted_regression.fit(x_train,y_train)
+    y_pred = locally_weighted_regression.predict(x_test)
+    mean_squared_error = ((y_test-y_pred) ** 2).mean()
+    plot(tau, mean_squared_error, x_train, y_train, x_valid, y_pred, test_set=True)
+    np.savetxt(pred_path, y_pred)
+
+
     # *** END CODE HERE ***
+
+def plot(tau, mean_squared_error, x_train, y_train, x_valid, y_pred, * , test_set=False):
+    fig,ax = plt.subplots()
+    ax.plot(x_train, y_train, 'bx')
+    ax.plot(x_valid, y_pred, 'ro')
+    ax.set_title(f'tau = {tau}, MSE = {mean_squared_error}')
+    fig.savefig(f'output/p05c_tau_{tau}{"_final" if test_set else ""}.png')
